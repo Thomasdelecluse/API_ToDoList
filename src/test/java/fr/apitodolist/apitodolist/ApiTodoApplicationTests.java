5140fa357051f3f8ee5035164b6f252de96f3b6d
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2023-03-28 00:11:51 +0200

    feat: add test

---
+++ src/test/java/fr/apitodolist/apitodolist/ApiTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.apitodolist.apitodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
