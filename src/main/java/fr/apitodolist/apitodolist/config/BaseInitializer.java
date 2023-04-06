843f65960264d4882867c8cc8027fda9e8d8c59c
ThomasDelecluseRedlab <81568452+Thomasdelecluse@users.noreply.github.com>
2023-04-06 17:45:36 +0200

    refactor: BaseInitializer in package config

--- src/main/java/fr/apitodolist/apitodolist/config/BaseInitializer.java
+++ src/main/java/fr/apitodolist/apitodolist/config/BaseInitializer.java
@@ -1,16 +1,19 @@
-package fr.apitodolist.apitodolist;
+package fr.apitodolist.apitodolist.config;
 
 import fr.apitodolist.apitodolist.modele.Utilisateur;
 import fr.apitodolist.apitodolist.repository.IUtilisateurRepository;
+import fr.apitodolist.apitodolist.service.impl.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 @Configuration
 public class BaseInitializer {
-
+    private static final Logger logger = Logger.getLogger(CustomUserDetailsService.class.getName());
     @Autowired
     IUtilisateurRepository iUtilisateurRepository;
 
@@ -21,7 +24,7 @@ public class BaseInitializer {
             iUtilisateurRepository.save(user);
             Utilisateur admin = new Utilisateur("admin","admin",true);
             iUtilisateurRepository.save(admin);
-            System.out.println("utilisateur crée");
+            logger.info("utilisateur crée");
         };
     }
 }
