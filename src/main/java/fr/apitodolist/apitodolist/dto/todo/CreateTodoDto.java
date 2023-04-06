1d3eeb7493d67280ab719ee918b2548aae658c05
ThomasDelecluseRedlab <81568452+Thomasdelecluse@users.noreply.github.com>
2023-04-06 17:44:47 +0200

    refactor: change package dto

--- src/main/java/fr/apitodolist/apitodolist/dto/todo/CreateTodoDto.java
+++ src/main/java/fr/apitodolist/apitodolist/dto/todo/CreateTodoDto.java
@@ -1,4 +1,4 @@
-package fr.apitodolist.apitodolist.dto;
+package fr.apitodolist.apitodolist.dto.todo;
 
 
 public record CreateTodoDto(String title, String description, String type, boolean status) {
