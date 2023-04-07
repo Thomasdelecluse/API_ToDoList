d5c4ec079b89e27197bfaf2ff64cfa3768413c7b
ThomasDelecluseRedlab <81568452+Thomasdelecluse@users.noreply.github.com>
2023-04-07 17:14:10 +0200

    feat: add filter

--- src/main/java/fr/apitodolist/apitodolist/config/SecurityConfig.java
+++ src/main/java/fr/apitodolist/apitodolist/config/SecurityConfig.java
@@ -1,38 +1,47 @@
 package fr.apitodolist.apitodolist.config;
 
+import fr.apitodolist.apitodolist.config.constant.JwtAuthenticationFilter;
 import fr.apitodolist.apitodolist.service.impl.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+    @Autowired
+    private AuthenticationManagerBuilder authenticationManagerBuilder;
 
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return authenticationManagerBuilder.build();
+    }
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
+                .addFilter(new JwtAuthenticationFilter(authenticationManager))
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.POST, "/utilisateur")
                 .permitAll()
                 .requestMatchers(HttpMethod.GET,"/test")
                 .hasRole("ADMIN")
                 .anyRequest().hasRole("USER")
-                .and()
-                .httpBasic()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
