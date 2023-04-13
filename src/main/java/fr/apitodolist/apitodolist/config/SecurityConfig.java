d80dec5fb615f2e87c8a41b407ce27a4c79e4e54
Thomasdelecluse <81568452+Thomasdelecluse@users.noreply.github.com>
2023-04-14 00:47:48 +0200

    feat: configure cors

--- src/main/java/fr/apitodolist/apitodolist/config/SecurityConfig.java
+++ src/main/java/fr/apitodolist/apitodolist/config/SecurityConfig.java
@@ -18,8 +18,12 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.crypto.SecretKey;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -31,11 +35,12 @@ public class SecurityConfig {
     @Autowired
     private JwtTokens jwtTokens;
 
+    //desactive la securité csrf et le cors pour accepter les requetes de n'importe quel domaine
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
         return http
-                .csrf().disable()
+                .cors().and().csrf().disable()
                         .addFilter(new JwtAuthenticationFilter(authenticationManagerBuilder.getObject(), jwtTokens, secretKey))
                         .addFilter(new JwtAuthorizationFilter(authenticationManagerBuilder.getObject(), jwtTokens, secretKey))
                 .authorizeHttpRequests()
@@ -53,6 +58,19 @@ public class SecurityConfig {
                             .build();
     }
 
+    // Configuration de la gestion des CORS
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
